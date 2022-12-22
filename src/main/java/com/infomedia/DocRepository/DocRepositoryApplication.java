0cc279d30a38872c8fbfc3e440ad71c26c32218e
Sergio Cruz <vlad.crz@gmail.com>
2022-12-21 18:20:10 -0600

    Firsts commit

---
+++ src/main/java/com/infomedia/DocRepository/DocRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.infomedia.DocRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infomedia/DocRepository/DocRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infomedia.DocRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
